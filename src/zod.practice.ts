5858cf8ca5dd407c305c135d6bd0617653600aea
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2023-01-18 17:44:28 -0300

    feat: base zod

--- src/zod.practice.ts
+++ src/zod.practice.ts
@@ -1,9 +1,17 @@
-import { z } from "zod";
+import { z, ZodError } from "zod";
 
 const User = z.object({
-    name: z.string(),
+    name: z.string().min(20, { message: "Precisa de no mínimo 20 caracteres" }),
     age: z.number()
 });
 
+type iUser = z.infer<typeof User>;
 
+const newUser: iUser = {
+    name: "Gabriel Henrique",
+    age: 24
+};
 
+User.parse(newUser);
+
+console.log(newUser);
